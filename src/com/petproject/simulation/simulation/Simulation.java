7d983085f5e0f458162a7de72a89ce52dc67f956
GooDkoof <MyNameIsNob0dy@yandex.ru>
2026-03-11 23:07:06 +0300

    удалил лишней метод

--- src/com/petproject/simulation/simulation/Simulation.java
+++ src/com/petproject/simulation/simulation/Simulation.java
@@ -187,27 +187,7 @@ public class Simulation {
         gameStats();
     }
 
-    private void clearConsole() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls")
-                        .inheritIO()
-                        .start()
-                        .waitFor();
-
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            }
-        } catch (Exception e) {
-            for (int i = 0; i < 50; i++) {
-                System.out.println();
-            }
-        }
-    }
-
     private void gameStats() {
-        clearConsole();
         mapConsoleRenderer.renderWorld(worldMap);
         GameMessenger.showStatus(worldMap, currentTurn);
     }
